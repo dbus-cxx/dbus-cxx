f85c96cb05eba7060d31feb9d909ec529bb8f70c
Robert Middleton <rm5248@users.noreply.github.com>
2012-11-19 02:52:18 +0000

    updated M4 script for c++0x compilation.  Updated DBusCxxPointer to error out if no smart pointers were found.  Updated configure.ac to properly check for c++0x smart pointers

--- dbus-cxx/dbus-cxx/pointer.h
+++ dbus-cxx/dbus-cxx/pointer.h
@@ -21,7 +21,7 @@
 #ifndef DBUS_CXXPOINTER
 #define DBUS_CXXPOINTER
   // Headers for smart pointers
-  #ifdef DBUS_CXX_USE_BOOST_SMART_POINTER
+  #if defined( DBUS_CXX_USE_BOOST_SMART_POINTER )
     #include <boost/shared_ptr.hpp>
     #include <boost/weak_ptr.hpp>
     #define DBusCxxPointer boost::shared_ptr
@@ -29,32 +29,32 @@
     #define dbus_cxx_static_pointer_cast  boost::static_pointer_cast
     #define dbus_cxx_const_pointer_cast   boost::const_pointer_cast
     #define dbus_cxx_dynamic_pointer_cast boost::dynamic_pointer_cast
-  #else
-    #ifdef DBUS_CXX_USE_CXX0X_SMART_POINTER
-      #include <memory>
-      #define DBusCxxPointer std::shared_ptr
-      #define DBusCxxWeakPointer std::weak_ptr
-      #define dbus_cxx_static_pointer_cast  std::static_pointer_cast
-      #define dbus_cxx_const_pointer_cast   std::const_pointer_cast
-      #define dbus_cxx_dynamic_pointer_cast std::dynamic_pointer_cast
+  #elif defined( DBUS_CXX_USE_CXX0X_SMART_POINTER )
+    #include <memory>
+    #define DBusCxxPointer std::shared_ptr
+    #define DBusCxxWeakPointer std::weak_ptr
+    #define dbus_cxx_static_pointer_cast  std::static_pointer_cast
+    #define dbus_cxx_const_pointer_cast   std::const_pointer_cast
+    #define dbus_cxx_dynamic_pointer_cast std::dynamic_pointer_cast
+  #elif defined( DBUS_CXX_USE_TR1_SMART_POINTER )
+    #include <typeinfo>
+    #include <memory>
+    #include <functional>
+    #if __GNUC__ > 4 || (__GNUC__ == 4 && (__GNUC_MINOR__ > 1 ))
+      #include <ext/concurrence.h>
+      #include <tr1/type_traits>
     #else
-      #include <typeinfo>
-      #include <memory>
-      #include <functional>
-      #if __GNUC__ > 4 || (__GNUC__ == 4 && (__GNUC_MINOR__ > 1 ))
-        #include <ext/concurrence.h>
-        #include <tr1/type_traits>
-      #else
-        #include <bits/concurrence.h>
-      #endif
-      #include <ext/mt_allocator.h>
-      #include <tr1/boost_shared_ptr.h>
-      #define DBusCxxPointer std::tr1::shared_ptr
-      #define DBusCxxWeakPointer std::tr1::weak_ptr
-      #define dbus_cxx_static_pointer_cast  std::tr1::static_pointer_cast
-      #define dbus_cxx_const_pointer_cast   std::tr1::const_pointer_cast
-      #define dbus_cxx_dynamic_pointer_cast std::tr1::dynamic_pointer_cast
+      #include <bits/concurrence.h>
     #endif
+    #include <ext/mt_allocator.h>
+    #include <tr1/boost_shared_ptr.h>
+    #define DBusCxxPointer std::tr1::shared_ptr
+    #define DBusCxxWeakPointer std::tr1::weak_ptr
+    #define dbus_cxx_static_pointer_cast  std::tr1::static_pointer_cast
+    #define dbus_cxx_const_pointer_cast   std::tr1::const_pointer_cast
+    #define dbus_cxx_dynamic_pointer_cast std::tr1::dynamic_pointer_cast
+  #else
+    #error "Could not find any available smart pointers to use"
   #endif
 
 #endif
