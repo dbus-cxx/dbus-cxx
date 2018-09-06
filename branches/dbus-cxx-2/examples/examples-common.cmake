0bd575b778014cda30f061b20df3939e5f461b01
rm5248 <rm5248@6008df8a-bab6-470b-9db8-c0eedfea88be>
2018-09-06 17:52:47 +0000

    examples running under cmake

--- branches/dbus-cxx-2/examples/examples-common.cmake
+++ branches/dbus-cxx-2/examples/examples-common.cmake
@@ -1,7 +1,3 @@
-include( FindPkgConfig )
-pkg_check_modules( dbus REQUIRED dbus-1>=1.3 )
-pkg_check_modules( sigc REQUIRED sigc++-2.0 )
-
 set( EXAMPLES_INCLUDES ${dbus_INCLUDE_DIRS} ${sigc_INCLUDE_DIRS} )
 set( EXAMPLES_LINK dbus-cxx ${dbus_LDFLAGS} ${sigc_LDFLAGS} -lrt )
 
