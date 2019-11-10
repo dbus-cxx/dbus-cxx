f3c1164aa9efbbfd23d0322af2ef9570f043dc25
Robert Middleton <robert.middleton@rm5248.com>
2019-11-09 19:18:12 -0500

    Added example for a struct

---
+++ examples/basics/struct/struct_client.cpp
@@ -0,0 +1,45 @@
+/***************************************************************************
+ *   Copyright (C) 2019 by Robert Middleton.                               *
+ *   robert.middleton@rm5248.com                                           *
+ *                                                                         *
+ *   This file is part of the dbus-cxx library.                            *
+ *                                                                         *
+ *   The dbus-cxx library is free software; you can redistribute it and/or *
+ *   modify it under the terms of the GNU General Public License           *
+ *   version 3 as published by the Free Software Foundation.               *
+ *                                                                         *
+ *   The dbus-cxx library is distributed in the hope that it will be       *
+ *   useful, but WITHOUT ANY WARRANTY; without even the implied warranty   *
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU   *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this software. If not see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#include <dbus-cxx.h>
+
+
+int main(int argc, const char** argv)
+{
+
+  DBus::init();
+
+  std::shared_ptr<DBus::Dispatcher> dispatcher = DBus::Dispatcher::create();
+
+  std::shared_ptr<DBus::Connection> connection = dispatcher->create_connection( DBus::BusType::SESSION );
+
+  std::shared_ptr<DBus::ObjectProxy> object = connection->create_object_proxy("dbuscxx.example.struct.server", "/dbuscxx/example/Struct");
+
+  DBus::MethodProxy<std::tuple<int,int>()>& methodref = *(object->create_method<std::tuple<int,int>()>("Struct.Basic", "now"));
+
+  std::tuple<int,int> value;
+  value = methodref( );
+  
+  char format_string[200];
+  
+  snprintf(format_string, 200, "%d %d", std::get<0>(value), std::get<1>(value) );
+  
+  std::cout << "The values are: " << format_string << std::endl;
+
+  return 0;
+}
---
+++ examples/basics/struct/struct_server.cpp
@@ -0,0 +1,61 @@
+/***************************************************************************
+ *   Copyright (C) 2019 by Robert Middleton.                               *
+ *   robert.middleton@rm5248.com                                           *
+ *                                                                         *
+ *   This file is part of the dbus-cxx library.                            *
+ *                                                                         *
+ *   The dbus-cxx library is free software; you can redistribute it and/or *
+ *   modify it under the terms of the GNU General Public License           *
+ *   version 3 as published by the Free Software Foundation.               *
+ *                                                                         *
+ *   The dbus-cxx library is distributed in the hope that it will be       *
+ *   useful, but WITHOUT ANY WARRANTY; without even the implied warranty   *
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU   *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this software. If not see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#include <dbus-cxx.h>
+#include <iostream>
+#include <sys/time.h>
+#include <unistd.h>
+
+
+std::tuple<int,int> now() {
+  std::tuple<int,int> value = std::make_tuple( 5, 88 );
+  return value;
+}
+
+int main()
+{
+  DBus::init();
+  
+  int ret;
+  
+  std::shared_ptr<DBus::Dispatcher> dispatcher = DBus::Dispatcher::create();
+
+  std::shared_ptr<DBus::Connection> conn = dispatcher->create_connection(DBus::BusType::SESSION);
+  
+  // request a name on the bus
+  ret = conn->request_name( "dbuscxx.example.struct.server", DBUS_NAME_FLAG_REPLACE_EXISTING );
+  if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) return 1;
+
+  std::shared_ptr<DBus::Object> object = conn->create_object("/dbuscxx/example/Struct");
+
+  object->create_method<std::tuple<int,int>()>("now", sigc::ptr_fun(now) );
+
+  std::cout << "Running" << std::flush;
+  
+  for (int i=0; i < 20; i++)
+  {
+    std::cout << "." << std::flush;
+    sleep(1);
+  }
+  
+  std::cout << std::endl;
+
+  return 0;
+
+}
