1d4fd866134363075d840f838b24bf83ea268106
Robert Middleton <robert.middleton@rm5248.com>
2020-03-20 15:27:43 -0400

    Updated adapter code

--- unit-tests/tools-tests/taco-adapter.cpp
+++ unit-tests/tools-tests/taco-adapter.cpp
@@ -1,2 +1,17 @@
+#include "DbusTacoAdapter.h"
+
+class TacoAdaptee : public DbusTacoAdaptee {
+    std::string OrderTaco(std::string meatType ) {
+        return "ordering " + meatType;
+    }
+};
+
 int main(){
+    DBus::init();
+
+    std::shared_ptr<DBus::Dispatcher> dispatch = DBus::Dispatcher::create();
+    std::shared_ptr<DBus::Connection> conn = dispatch->create_connection( DBus::BusType::SESSION );
+
+    TacoAdaptee taco;
+    std::shared_ptr<DbusTacoAdapter> ptr = DbusTacoAdapter::create( &taco );
 }
