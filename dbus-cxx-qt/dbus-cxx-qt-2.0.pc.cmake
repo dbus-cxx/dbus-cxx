10b6e2e2ef0f53a8adac1910beffa6ea9b13133f
Robert Middleton <robert.middleton@rm5248.com>
2020-05-17 22:08:56 -0400

    Updated installation for Qt support

---
+++ dbus-cxx-qt/dbus-cxx-qt-2.0.pc.cmake
@@ -0,0 +1,9 @@
+Name: dbus-cxx-qt
+Description: C++ bindings for dbus(Qt support)
+Version: ${PKG_VERSION}
+Requires: ${PKG_CONFIG_REQUIRES}
+prefix=${CMAKE_INSTALL_PREFIX}
+includedir=${PKG_CONFIG_INCLUDEDIR}
+libdir=${PKG_CONFIG_LIBDIR}
+Libs: ${PKG_CONFIG_LIBS}
+Cflags: ${PKG_CONFIG_CFLAGS}
