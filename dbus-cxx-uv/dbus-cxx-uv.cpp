8cdfbf6f5aa08743f51817da1f7ffda0d423db23
ErikHons <erik.hons@ni.com>
2023-08-10 16:20:01 -0500

    Early Look: Add libuv integration (#121)
    * Add libuv integration
    
    * Update dbus-cxx-uv/CMakeLists.txt
    
    Co-authored-by: alynch-ni <alynch-ni@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Robert Middleton <rm5248@users.noreply.github.com>
    Co-authored-by: alynch-ni <alynch-ni@users.noreply.github.com>

---
+++ dbus-cxx-uv/dbus-cxx-uv.cpp
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later OR BSD-3-Clause
+// SPDX-License-Identifier: LGPL-3.0-or-later OR BSD-3-Clause
+/***************************************************************************
+ *   Copyright (C) 2020 by Robert Middleton                                *
+ *   robert.middleton@rm5248.com                                           *
+ *                                                                         *
+ *   This file is part of the dbus-cxx library.                            *
+ *                                                                         *
+ *   The dbus-cxx library is free software; you can redistribute it and/or *
+ *   modify it under the terms of the GNU General Public License           *
+ *   version 3 as published by the Free Software Foundation.               *
+ *                                                                         *
+ *   The dbus-cxx library is distributed in the hope that it will be       *
+ *   useful, but WITHOUT ANY WARRANTY; without even the implied warranty   *
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU   *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this software. If not see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#include "dbus-cxx-uv.h"
