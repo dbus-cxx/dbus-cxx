b96df1a2755e3c3b23f3dee034f7316c7d87158c
Jørn Gustav Larsen <164860481+jglanycon@users.noreply.github.com>
2024-08-27 03:47:47 +0200

    Extra mutexs has been  added, for overcoming problems when using the library in multithreaded applications. (#154)
    Co-authored-by: Jørn Gustav Larsen <jgl@adminbyrequest.com>

--- dbus-cxx/sendmsgtransport.cpp
+++ dbus-cxx/sendmsgtransport.cpp
@@ -15,6 +15,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <mutex>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -171,6 +172,9 @@ public:
     void* tx_control_data;
     int tx_control_capacity;
 
+    std::mutex m_receive_mutex;
+    std::mutex m_send_mutex;
+
     void init() {
         // Setup the RX data msghdr
         rx_msg.msg_iov = &rx_buf;
@@ -258,7 +262,9 @@ std::shared_ptr<SendmsgTransport> SendmsgTransport::create( int fd, bool initial
     return std::shared_ptr<SendmsgTransport>( new SendmsgTransport( fd, initialize ) );
 }
 
-ssize_t SendmsgTransport::writeMessage( std::shared_ptr<const DBus::Message> message, uint32_t serial ) {
+ssize_t SendmsgTransport::writeMessage( std::shared_ptr<const DBus::Message> message, uint32_t serial )
+{
+    std::scoped_lock lock(m_priv->m_send_mutex);
 #ifdef _WIN32
     const std::vector<int> filedescriptors = message->filedescriptors();
 
@@ -342,6 +348,7 @@ ssize_t SendmsgTransport::writeMessage( std::shared_ptr<const DBus::Message> mes
 }
 
 std::shared_ptr<DBus::Message> SendmsgTransport::readMessage() {
+    std::scoped_lock lock(m_priv->m_receive_mutex);
     uint32_t header_array_len;
     ssize_t body_len;
     ssize_t total_len;
